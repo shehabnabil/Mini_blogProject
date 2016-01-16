13563302a75e8dae4561e88f25c8db55b48b86c5
Joe Spampinato <jcspampinato@gmail.com>
2016-01-15 19:08:54 -0500

    fixes #3. Also database reset

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110145829) do
+ActiveRecord::Schema.define(version: 20151030214459) do
 
   create_table "follows", force: :cascade do |t|
     t.integer "follower_id"
